a4ab5e58a5a47fe2dae0b79504885601eb697c2d
Dian Chen <dianchen96@gmail.com>
2018-09-06 18:36:17 -0500

    following ball partially done

--- core/python/behaviors/hw1_gaze.py
+++ core/python/behaviors/hw1_gaze.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+import memory
 import mem_objects
 import core
 import commands
@@ -12,29 +13,47 @@ from task import Task
 from state_machine import Node, S, C, T, LoopingStateMachine
 import UTdebug
 
-CENTER_THRESHOLD = 50
+CENTER_THRESHOLD = 100
+DELAY = 0.6
+
+class Stand(Node):
+        def run(self):
+            commands.stand()
+            if self.getTime() > 3.0:
+                # memory.speech.say("playing stand complete")
+                self.finish()
+
 
 class GazeCenter(Node):
 	def run(self):
-		pass
+		self.finish()
 
 class GazeLeft(Node):
 	def run(self):
-		commands.setHeadPan(0.5, 2.0, True)
-
+		commands.setHeadPan(0.1, DELAY, True)
+		if self.getTime() > DELAY + 0.2:
+                	self.finish()
+		# commands.setWalkVelocity(0.5, 0, 0)
 
 class GazeRight(Node):
 	def run(self):
-		commands.setHeadPan(-0.5, 2.0, True)
+		commands.setHeadPan(-0.1, DELAY, True)
+                if self.getTime() > DELAY + 0.2:
+                        self.finish()
 
 class GazeTop(Node):
 	def run(self):
 		commands.setHeadTilt(-20)
+		if self.getTime() > DELAY:
+                        self.finish()
 
 class GazeBottom(Node):
 	def run(self):
+		# print ("bottom")
 		commands.setHeadTilt(-22)
-
+		if self.getTime() > DELAY:
+                        self.finish()
+		# commands.setHeadPan(1.0, 2.0)
 
 class Gazer(Node):
 	def run(self):
@@ -53,14 +72,14 @@ class Gazer(Node):
 			elif x_diff >= CENTER_THRESHOLD:
 				choice = "right"
 			elif y_diff <= -CENTER_THRESHOLD:
-				choice = "top"
+			 	choice = "top"
 			elif y_diff >= CENTER_THRESHOLD:
-				choice = "bottom"
+			 	choice = "bottom"
 
 		else:
 			choice = "no_ball"
 			print ("No ball detected")
-		
+		print (choice)
 		self.postSignal(choice)
 
 
@@ -68,6 +87,7 @@ class Gazer(Node):
 class Playing(LoopingStateMachine):
 	def setup(self):
 		gazer = Gazer()
+		stand = Stand()
 		gazes = {
 			'center': GazeCenter(),
 			'left': GazeLeft(),
@@ -75,7 +95,12 @@ class Playing(LoopingStateMachine):
 			'top': GazeTop(),
 			'bottom': GazeBottom(),
 		}
+		self.add_transition(stand, C, gazer)
 		for name in gazes:
-			self.add_transition(gazer, S(name), gazes[name], T(5), gazer)
+			self.add_transition(gazer, S(name), gazes[name], C, gazer)
 
-		self.add_transition(gazer, S('no_ball'), gazer)
+		# self.add_transition(gazer, S('no_ball'), gazer)
+		# stand = Stand()
+		# gaze1 = GazeLeft()
+		# gaze2 = GazeLeft()
+		# self.trans(stand, C, gaze1, T(3.0), gaze2, T(3.0))
