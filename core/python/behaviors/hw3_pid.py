a07ac86c9ba7d41bdf553e9b4cbc7254e6f5726c
Abheek Ghosh <abheekghosh.ag@gmail.com>
2018-09-28 17:05:11 -0500

    ball following and align

--- core/python/behaviors/hw3_pid.py
+++ core/python/behaviors/hw3_pid.py
@@ -14,22 +14,27 @@ from task import Task
 from state_machine import Node, S, C, T, LoopingStateMachine
 import UTdebug
 
-DELAY = 0.1
-DIST_THRESHOLD = 240
-VX_MIN = 0.01
-VX_MAX = 1
-VY_MIN = 0.01
-VTHETA_MIN = 0.
-VTHETA_MAX = 0.25
-DISCOUNT_FACTOR = 0.1
+DELAY = 0.05
+BALL_MIN = 100
+GOAL_MIN = 500
 
-P = 1
-I = 0.0
-D = 0.1
+VX_MIN = 0.1
+VX_MAX = 1
+VY_MIN = 0.1
+VTHETA_MIN = 0.1
+VTHETA_MAX = 0.25
+
+FACTOR = 0.01
+SCALE = 0.005
+
+P = 1.
+I = 0.
+D = 0.
 
 vx = 0.
 vy = 0.
 vtheta = 0.
+goal_side = 1
 
 
 class Controller(object):
@@ -71,6 +76,7 @@ class Controller(object):
 class Stand(Node):
     def run(self):
         commands.stand()
+        # commands.setHeadPanTilt(pan=0, tilt=0, time=0.5, isChange=True)
         if self.getTime() > 3.0:
             memory.speech.say("stand up")
             self.finish()
@@ -92,6 +98,9 @@ class FindBall(Node):
         global vx
         global vy
         global vtheta
+        global goal_side
+
+        vx0, vy0, vtheta0 = vx, vy, vtheta
 
         ball = mem_objects.world_objects[core.WO_BALL]
         goal = mem_objects.world_objects[core.WO_UNKNOWN_GOAL]
@@ -102,31 +111,42 @@ class FindBall(Node):
         goal_theta = goal.visionBearing
         goal_x = goal.visionDistance
 
+        # Yajie's goal seen earlier code
+        if goal.seen:
+            goal_side = goal_theta / abs(goal_theta)
+            # print ('Goal Side  %d' % goal_side )
+
         if not ball.seen:
             print ('\n\n\n\nBall not seen.\n\n\n\n')
-            vx = DISCOUNT_FACTOR*vx
-            vy = DISCOUNT_FACTOR*vy
-            vtheta = DISCOUNT_FACTOR*vtheta
-        elif ball.fromTopCamera or ball_x > DIST_THRESHOLD:
+            vx = 0.0
+            # vy = FACTOR*vy
+            # vtheta = FACTOR*vtheta
+            vy = 0.0
+            vtheta = 0.5
+        elif ball.fromTopCamera and ball_x > BALL_MIN:
             print ('\n\n\n\nBall seen far away.\n\n\n\n')
-            vx = self.controller(min( (ball_x - DIST_THRESHOLD)/100, VX_MAX))
+            vx = self.controller(min( (ball_x - BALL_MIN) * SCALE, VX_MAX))
             vtheta = min ( (abs(ball_theta) > VTHETA_MIN)*abs(ball_theta), VTHETA_MAX
                 ) * (ball_theta/abs(ball_theta))
         else:
-            vx = 0
+            vx = 0.1
             if goal.seen:
                 print ('\n\n\n\nGoal visible.\n\n\n\n')
-                vy = ball_theta
-                vtheta = ball_theta-goal_theta
+                vy = -ball_theta
+                vtheta = (ball_theta-goal_theta)
             else:
-                print ('\n\n\n\nGoal NOT visible.\n\n\n\n')
-                vy = 0.5
+                print ('\n\n\n\nGoal NOT visible. goal side: ', goal_side, "\n\n\n\n")
+                vy = -0.5 * goal_side
                 vtheta = -0.5*vy
 
 
-        vx = vx * (vx > VX_MIN)
-        vy = vy * (vy > VY_MIN)
-        vtheta = vtheta * (vtheta > VTHETA_MIN)
+        vx += FACTOR*(vx-vx0);
+        vy += FACTOR*(vy-vy0);
+        vtheta += FACTOR*(vtheta-vtheta0);
+
+        vx *= (abs(vx) > VX_MIN)
+        vy *=  (abs(vy) > VY_MIN)
+        vtheta *=  (abs(vtheta) > VTHETA_MIN)
 
         self.finish()
 
