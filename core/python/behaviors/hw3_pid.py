e152f0737b3cd912a90524a94dd99742bfeeeffd
Abheek Ghosh <abheekghosh.ag@gmail.com>
2018-09-28 18:25:27 -0500

    dribbling OK, but needs tuning

--- core/python/behaviors/hw3_pid.py
+++ core/python/behaviors/hw3_pid.py
@@ -18,13 +18,13 @@ DELAY = 0.05
 BALL_MIN = 100
 GOAL_MIN = 500
 
-VX_MIN = 0.1
+VX_MIN = 0.2
 VX_MAX = 1
 VY_MIN = 0.1
-VTHETA_MIN = 0.1
+VTHETA_MIN = 0.15
 VTHETA_MAX = 0.25
 
-FACTOR = 0.01
+FACTOR = 0.001
 SCALE = 0.005
 
 P = 1.
@@ -34,7 +34,10 @@ D = 0.
 vx = 0.
 vy = 0.
 vtheta = 0.
+
 goal_side = 1
+ball_side = 1
+dribble = 0
 
 
 class Controller(object):
@@ -76,7 +79,7 @@ class Controller(object):
 class Stand(Node):
     def run(self):
         commands.stand()
-        # commands.setHeadPanTilt(pan=0, tilt=0, time=0.5, isChange=True)
+        commands.setHeadPanTilt(pan=0, tilt=0, time=0.5, isChange=True)
         if self.getTime() > 3.0:
             memory.speech.say("stand up")
             self.finish()
@@ -99,6 +102,17 @@ class FindBall(Node):
         global vy
         global vtheta
         global goal_side
+        global ball_side
+        global dribble
+
+        if dribble > 0:
+            print ("\n\n\n\ndribble\n\n\n\n")
+            dribble -= DELAY
+            vx = 1
+            vy = 0
+            vtheta = 0
+            self.finish()
+            return
 
         vx0, vy0, vtheta0 = vx, vy, vtheta
 
@@ -111,10 +125,13 @@ class FindBall(Node):
         goal_theta = goal.visionBearing
         goal_x = goal.visionDistance
 
-        # Yajie's goal seen earlier code
+        # Goal seen earlier code
         if goal.seen:
             goal_side = goal_theta / abs(goal_theta)
-            # print ('Goal Side  %d' % goal_side )
+        
+        if ball.seen:
+            ball_side = ball_theta / abs(ball_theta)
+            
 
         if not ball.seen:
             print ('\n\n\n\nBall not seen.\n\n\n\n')
@@ -122,28 +139,33 @@ class FindBall(Node):
             # vy = FACTOR*vy
             # vtheta = FACTOR*vtheta
             vy = 0.0
-            vtheta = 0.5
+            vtheta = ball_side* 0.5
         elif ball.fromTopCamera and ball_x > BALL_MIN:
             print ('\n\n\n\nBall seen far away.\n\n\n\n')
             vx = self.controller(min( (ball_x - BALL_MIN) * SCALE, VX_MAX))
             vtheta = min ( (abs(ball_theta) > VTHETA_MIN)*abs(ball_theta), VTHETA_MAX
                 ) * (ball_theta/abs(ball_theta))
         else:
-            vx = 0.1
+            vx = 0.
             if goal.seen:
                 print ('\n\n\n\nGoal visible.\n\n\n\n')
-                vy = -ball_theta
+                vy = -ball_theta*0.5
                 vtheta = (ball_theta-goal_theta)
             else:
                 print ('\n\n\n\nGoal NOT visible. goal side: ', goal_side, "\n\n\n\n")
-                vy = -0.5 * goal_side
-                vtheta = -0.5*vy
+                vy = -goal_side* 0.5
+                vtheta = -0.3*vy
+
+            if abs(vy) + abs(vtheta) < VY_MIN + VTHETA_MIN:
+                dribble = 1.
 
 
+        # Moving average
         vx += FACTOR*(vx-vx0);
         vy += FACTOR*(vy-vy0);
         vtheta += FACTOR*(vtheta-vtheta0);
 
+        # Just a check to stop the robot when values close to 0. Not very important.
         vx *= (abs(vx) > VX_MIN)
         vy *=  (abs(vy) > VY_MIN)
         vtheta *=  (abs(vtheta) > VTHETA_MIN)
