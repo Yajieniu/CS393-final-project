4ef6c17ad02b44d66d79ec9607dad0dd3a5e23c7
Dian Chen <dianchen96@gmail.com>
2018-10-03 13:02:34 -0500

    kick fixing

--- core/python/behaviors/hw3_final.py
+++ core/python/behaviors/hw3_final.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 from math import pi
+from time import sleep
 
 import memory
 import mem_objects
@@ -15,17 +16,17 @@ from state_machine import Node, S, C, T, LoopingStateMachine
 import UTdebug
 
 THETA_THRESHOLD = 0.05
-RIGHT_FOOT_OFFSET = -0.20
+RIGHT_FOOT_OFFSET = -0.25
 
 DELAY = 0.05
 BALL_MIN_PHASE1 = 250
-BALL_MIN_PHASE2 = 200
-GOAL_MIN = 500
+BALL_MIN_PHASE2 = 160
+GOAL_MIN = 1000
 
-VX_MIN = 0.2
+VX_MIN = 0.1
 VX_MAX = 1
-VY_MIN = 0.05
-VTHETA_MIN = 0.05
+VY_MIN = 0.1
+VTHETA_MIN = 0.1
 VTHETA_MAX = 0.25
 
 FACTOR = 0.001
@@ -33,7 +34,7 @@ SCALE = 0.005
 
 VX_OFFSET = 0.1
 
-P = 1#4e-3/SCALE
+P = 1#2e-3/SCALE
 I = 1e-4/SCALE
 D = 2e-4/SCALE
 
@@ -45,6 +46,8 @@ goal_side = 1
 ball_side = 1
 play_mode = 1
 dribble = 0.
+kick_mode = False
+kick_waiting = 3.0
 
 left_offset = 0.
 
@@ -96,11 +99,21 @@ class Stand(Node):
 
 class FollowBall(Node):
     def run(self):
-        if vx > VX_MIN:
-            print ("\n\n\t\t vx=%.3f, vy=%.3f, vtheta=%.3f" % (vx, vy, vtheta))
-        commands.setWalkVelocity(vx, vy, vtheta)
-
-
+        global play_mode
+        global kick_waiting
+        if not kick_mode and play_mode == 4: # Kick
+            frames = self.getFrames()
+            memory.walk_request.noWalk()
+            goal = mem_objects.world_objects[core.WO_UNKNOWN_GOAL]
+            print("**********goal distance: ", goal.visionDistance)
+            memory.kick_request.setFwdKick()
+            play_mode = 6
+            kick_waiting = 3.0
+            if self.getFrames() - frames > 150 and not memory.kick_request.kick_running_:
+                self.finish()
+                # play_mode = 1
+        elif play_mode != 6:
+            commands.setWalkVelocity(vx, vy, vtheta)
 
 class FindBall(Node):
     def __init__(self):
@@ -117,6 +130,8 @@ class FindBall(Node):
         global dribble
         global left_offset
         global play_mode
+        global kick_mode
+        global kick_waiting
         global FACTOR
 
         vx0, vy0, vtheta0 = vx, vy, vtheta
@@ -133,15 +148,15 @@ class FindBall(Node):
         # Goal seen earlier code
         if goal.seen:
             goal_side = goal_theta / abs(goal_theta)
+            if goal.fromTopCamera and goal_x < GOAL_MIN:
+                kick_mode = True
         
         if ball.seen:
             ball_side = ball_theta / abs(ball_theta)
-        
-        if play_mode != 4:
-            play_mode = 3
+
         # Changing speed
         if play_mode == 1:
-            FACTOR = 0.01
+            FACTOR = 0.2
             if not ball.seen:
                 print ('\n\n\n\nBall not seen.\n\n\n\n')
                 vx = 0.0
@@ -155,81 +170,113 @@ class FindBall(Node):
                 play_mode = 2
 
         elif play_mode == 2:
-            if ball.seen and (ball.fromTopCamera or ball_x > 2*BALL_MIN_PHASE1):
+            if not ball.seen or (ball.fromTopCamera or ball_x > 2*BALL_MIN_PHASE1):
                 play_mode = 1
 
             vx = 0.
             if goal.seen:
-                print ('\n\n\n\nGoal visible.\n\n\n\n')
-                vy = -ball_theta*0.3
-                vtheta = (ball_theta-goal_theta)
+                print ('\n\n\n\nGoal visible.')
+                # vx = 0.2
+                # vy = -ball_theta*0.5
+                # vtheta = (ball_theta-goal_theta)
+                if False:#abs(ball_theta) > 5*VY_MIN:
+                    print (' ball\n\n\n\n')
+                    vy = ball_theta
+                    vtheta = 0
+                else:
+                    print (' goal\n\n\n\n')
+                    vy = -goal_theta
+                    vtheta = -0.5*vy
             else:
                 print ('\n\n\n\nGoal NOT visible. goal side: ', goal_side, "\n\n\n\n")
                 vy = -goal_side* 0.5
                 vtheta = -0.5*vy
 
-            # if abs(vy) < 3*VY_MIN and abs(vtheta) < 3*VTHETA_MIN:
-
-            if goal_x < GOAL_MIN: # TODO: tune GOAL_MIN
-                # Kick time
-                play_mode = 3
-            else:
-                play_mode = 5
-                dribble = 1.
+            if abs(vy) < VY_MIN and abs(vtheta) < VTHETA_MIN:
+                if kick_mode:
+                    play_mode = 3
+                else:
+                    play_mode = 5
+                    dribble = 1.
 
         elif play_mode == 3:
+            if not ball.seen or (ball.fromTopCamera or ball_x > 2*BALL_MIN_PHASE1):
+                play_mode = 1
             # Stop and transfer to kick
             FACTOR = 1.
+            print ('\n\n\nPreparing for kick.\n\n\n\n')
             print ('\n\tball distance: %.3f\n'%ball_x)
-            vx = self.controller( (ball_x - BALL_MIN_PHASE2) * SCALE) + VX_OFFSET
-            vtheta = (ball_theta-goal_theta)
+            # vx = self.controller( (ball_x - BALL_MIN_PHASE2) * SCALE) + VX_OFFSET
+            vx = 0
+            vy = 0
+            vtheta = 0
+            if ball_x >= BALL_MIN_PHASE2:
+                vx = 0.32
+            if abs(ball_theta - RIGHT_FOOT_OFFSET) >= 0.05:
+                vy = ball_theta - RIGHT_FOOT_OFFSET
+                # vy = -ball_theta*0.5
+                # vtheta = (ball_theta-goal_theta)
+                # vtheta = -0.5*vy
 
-            if ball_x < BALL_MIN_PHASE2 and abs(vtheta) < VTHETA_MIN:
+            if ball_x < BALL_MIN_PHASE2 and abs(vy) < 0.05:
                 play_mode = 4
                 left_offset = 0.5
 
         elif play_mode == 4:
-            if left_offset > 0:
-                print ('moving left '*100)
-                left_offset -= DELAY
-                vy = 0.3
-                vx = 0.5
-            else:
-                vx = 0
-                vy = 0
-
-            # memory.walk_request.noWalk()
-            # memory.kick_request.setFwdKick()
-
-            # if not memory.kick_request.kick_running_:
-                # play_mode = 1
-            # vx = 0
+            # if left_offset > 0:
+            #     print ('moving left '*100)
+            #     left_offset -= DELAY
+            #     vy = 0.3
+            #     vx = 0.25
+            # elif left_offset > -1:
+            #     left_offset -= DELAY
+            # else:
+            #     vx = 0
+            #     vy = 0
+            #     kick_mode = False
+            print ("\n\tkick!!\n")
+            vx = 0
+            vy = 0
             vtheta = 0
+            kick_mode = False
+            if not ball.seen or (ball.fromTopCamera or ball_x > 2*BALL_MIN_PHASE1):
+                play_mode = 3
+                kick_mode = True
+            else:
+                sleep(0.5)
 
-        else:
+
+        elif play_mode == 5:
             if dribble > 0:
                 print ("\n\n\n\ndribble\n\n\n\n")
                 dribble -= DELAY
                 vx = 0.75
                 if ball.seen and goal.seen:
-                    # vy = -ball_theta*0.3
-                    vtheta = (ball_theta-goal_theta)
+                    vtheta = ball_theta
                 else:
                     vy = 0
                     vtheta = 0
             else:
                 play_mode = 1
+        else:
+            if kick_waiting > 0:
+                print ('\n\nWainting fo kick to finish\n\n');
+                kick_waiting -= DELAY
+            else:
+                play_mode = 1
 
 
-        # Moving average
+        print ("\n\n\t\t vx=%.3f, vy=%.3f, vtheta=%.3f" % (vx, vy, vtheta))
+
+        # Moving average        
         vx = vx0 + FACTOR*(vx-vx0);
         vy = vy0 + FACTOR*(vy-vy0);
         vtheta = vtheta0 + FACTOR*(vtheta-vtheta0);
 
         # Just a check to stop the robot when values close to 0. Not very important.
-        vx *= (abs(vx) > VX_MIN)
-        vy *=  (abs(vy) > VY_MIN)
-        vtheta *=  (abs(vtheta) > VTHETA_MIN)
+        # vx *= (abs(vx) > VX_MIN)
+        # vy *=  (abs(vy) > VY_MIN)
+        # vtheta *=  (abs(vtheta) > VTHETA_MIN)
 
         self.finish()
 
@@ -251,7 +298,7 @@ class Playing(LoopingStateMachine):
 
         goal_side = 1
         ball_side = 1
-        play_mode = 3
+        play_mode = 1
         dribble = 0.
 
         left_offset = 0.
