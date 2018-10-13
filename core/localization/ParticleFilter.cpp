747e843392a8cb4e71362862f7cd257387288893
yajieniu <fineyearr3714@gmail.com>
2018-10-12 20:09:19 -0500

    particle filter framework

--- core/localization/ParticleFilter.cpp
+++ core/localization/ParticleFilter.cpp
@@ -3,32 +3,129 @@
 #include <memory/OdometryBlock.h>
 #include <common/Random.h>
 
+
+/* 
+ * Create an instance of class Particle Filter. 
+ * Or to say create a particle filter. 
+ * Only needed to be called once at the beginning. 
+ */
 ParticleFilter::ParticleFilter(MemoryCache& cache, TextLogger*& tlogger) 
-  : cache_(cache), tlogger_(tlogger), dirty_(true) {
+  : cache_(cache), tlogger_(tlogger), dirty_(true), 
+    backToRandom(false), needToUpdate(false) {
 }
 
+
+/* 
+ * Allow the particles to be udpated in this frame. 
+ */
+void ParticleFilter::NeedUpdate() {
+  needToUpdate = true;
+}
+
+
+/* 
+ * Initialize the particle filter. 
+ * Does not make sense to me. 
+ */
 void ParticleFilter::init(Point2D loc, float orientation) {
   mean_.translation = loc;
   mean_.rotation = orientation;
 }
 
+/* 
+ * Update particles. 
+ * But this function is different from the Particle Filter 
+ * algorithm. 
+ */
 void ParticleFilter::processFrame() {
   // Indicate that the cached mean needs to be updated
   dirty_ = true;
 
   // Retrieve odometry update - how do we integrate this into the filter?
+  // This is the observation, z_t
+  // contains x, and y and theta
+  // But might need further translation
   const auto& disp = cache_.odometry->displacement;
   log(41, "Updating particles from odometry: %2.f,%2.f @ %2.2f", disp.translation.x, disp.translation.y, disp.rotation * RAD_T_DEG);
   
-  // Generate random particles for demonstration
-  particles().resize(100);
-  auto frame = cache_.frame_info->frame_id;
-  for(auto& p : particles()) {
-    p.x = Random::inst().sampleN() * 250 + (frame * 5); //static_cast<int>(frame * 5), 250);
-    p.y = Random::inst().sampleN() * 250; // 0., 250);
-    p.t = Random::inst().sampleN() * M_PI / 4;  //0., M_PI / 4);
-    p.w = Random::inst().sampleU();
+  // We have a fixed number of particles, for now.
+  // might need to add random samples to recovery from kidnapping,
+  // which Josiah said he would definitely do
+  particles().resize(numOfParticles); 
+
+  if (backToRandom) { // could get rid of this random part
+    // Generate random particles for demonstration
+    particles().resize(100);  // change the num of particles
+    auto frame = cache_.frame_info->frame_id;
+    for(auto& p : particles()) {
+      p.x = Random::inst().sampleN() * 250 + (frame * 5); //static_cast<int>(frame * 5), 250);
+      p.y = Random::inst().sampleN() * 250; // 0., 250);
+      p.t = Random::inst().sampleN() * M_PI / 4;  //0., M_PI / 4);
+      p.w = Random::inst().sampleU();
+    }
   }
+  else if (needToUpdate) {  // can only keep this part
+    PFAlgorithm();
+  }
+}
+
+
+/* 
+ * The Implementation of the Particle Filter algorithm. 
+ */
+void ParticleFilter::PFAlgorithm() {
+
+  // Getting the last particle set
+  std::vector<Particle> particles = particles();
+  std::vector<Particle> X = particles;
+  std::vector<Particle> X1 = particles;
+  Particle xtm;
+  float weights[numOfParticles] = {};
+  float totalWeight = 0;
+
+  int M = numOfParticles;
+
+  // First part of the algorithm
+  for (int i = 0; i < M; i++) {
+    xtm = sampling(ut, particles[i]); 
+    assert(weights[i] == 0);
+    totalWeight += getWeight(xtm);
+    X[i] = xtm;
+  }
+
+  // Instead of normalizing the weight, here is getting 
+  // the sum of all weights
+  weights[0] = X[0].w;
+  for (int i = 1; i < M; i++) {
+    weights[i] = weights[i-1] + X[i].w;
+  }
+
+  // Second part of the algorithm
+  for (int i = 0; i < M; i++) {
+    X1[i] = resampling();
+  }
+
+  // store the result back into memory
+  cache_.localization_mem->particles = X1;
+}
+
+
+/* Algorithm line 9. */
+Particle& ParticleFilter::resampling(std::vector<Particle>& particles, 
+  float (&weights)[numOfParticles]) {
+  Particle x = particles[0];
+  return x;
+}
+
+/* Algorithm line 5. */
+float ParticleFilter::getWeight(Particle & x) {
+  return x.w;
+}
+
+/* Algorithm line 4. */
+Particle& ParticleFilter::sampling(std::String& ut, Particle& xm) {
+  Particle x = xm;
+  return x;
 }
 
 const Pose2D& ParticleFilter::pose() const {
--- core/localization/ParticleFilter.h
+++ core/localization/ParticleFilter.h
@@ -5,11 +5,14 @@
 #include <memory/LocalizationBlock.h>
 #include <localization/Logging.h>
 
+#define numOfParticles = 3000;  // number of particles
+
 class ParticleFilter {
   public:
     ParticleFilter(MemoryCache& cache, TextLogger*& tlogger);
     void init(Point2D loc, float orientation);
     void processFrame();
+    void NeedUpdate();
     const Pose2D& pose() const;
     inline const std::vector<Particle>& particles() const {
       return cache_.localization_mem->particles;
@@ -21,9 +24,27 @@ class ParticleFilter {
     }
 
   private:
+    void PFAlgorithm();
+    Particle& sampling(String& ut, Particle& xm);
+    float getWeight(Particle & x); 
+    Particle& resampling(std::vector<Particle>& particles, 
+                float (&weights)[numOfParticles]);
+
+
     MemoryCache& cache_;
     TextLogger*& tlogger_;
 
     mutable Pose2D mean_;
     mutable bool dirty_;
+    mutable bool backToRandom;
+    mutable bool needToUpdate;
+    
+
+    String Controls[6] = ['forward', 
+                          'backward', 
+                          'left', 
+                          'right', 
+                          'turn_left',
+                          'turn_right'
+                          ]
 };
