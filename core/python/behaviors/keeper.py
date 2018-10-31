d526e40aa2e006f69938ff4cb34f179464e4ce20
Abheek Ghosh <abheekghosh.ag@gmail.com>
2018-10-31 10:27:39 -0500

    keeper movement

--- core/python/behaviors/keeper.py
+++ core/python/behaviors/keeper.py
@@ -13,201 +13,83 @@ from task import Task
 from state_machine import Node, S, C, T, LoopingStateMachine
 import UTdebug
 
-# Added in assignment 4:
+# Added in assignment 6:
 import math
 import pose
 import cfgpose
-
 import time
+import random
 
-# State models: (x, y, vx, vy)
+DELAY = 1.0
 
-# Measurements:
-# imageCenterX, imageCenterY, position p, visionBearing, 
-# visionDistance
+# class Stand(Node):
+#     def run(self):
+#         commands.stand()
+#         if self.getTime() > 3.0:
+#             memory.speech.say("stand up")
+#             self.finish()
 
-CENTER_MIN_THRESHOLD = 200
-CENTER_MAX_THRESHOLD = 200
-V_THRESHOLD = 35
-X_THRESHOLD = 600
-
-running_vx = 0.0
-running_vy = 0.0
-FACTOR = 0.4
-
-unseen_count = 0
-
-class RaiseLeft(Node):
-	# raise left arm 
+class TurnLeft(Node):
 	def run(self):
-		# UTdebug.log(15, "\n\n\n**********Raising left arm.")
-		print("\n\n\n***************************************\
-			************************************************\
-			*************\
-			Raising left arm.\n\n\n\n")
-		pose.RaiseLeftArm()
+		commands.setWalkVelocity(0, 0, 0.3);
 
-
-class RaiseRight(Node):
-	# raise right arm
+class TurnRight(Node):
 	def run(self):
-		# UTdebug.log(15, "\n\n**********Raising right arm.")
-		print("\n\n\n***************************************\
-			************************************************\
-			*************\
-			Raising right arm.\n\n\n\n")
-		pose.RaiseRightArm()
+		commands.setWalkVelocity(0, 0, -0.3);
 
-
-class RaiseBoth(Node):
-	# raise both arms
+class WalkFront(Node):
 	def run(self):
-		# UTdebug.log(15, "\n\n**********Raising both arms.")
-		print("\n\n\n***************************************\
-			************************************************\
-			*************\
-			Raising both arms.\n\n\n\n")		
-		pose.RaiseBothArms()
+		commands.setWalkVelocity(0.3, 0, 0);
 
-class NotSeen(Node):
+class WalkBack(Node):
 	def run(self):
-		commands.setHeadPan(0, 0.05)
-		if self.getTime() > 0.1:
-			self.finish()
+		commands.setWalkVelocity(-0.3, 0, 0);
 
-
-class Wait(Node):
+class WalkLeft(Node):
 	def run(self):
-		if self.getTime() > 1:
-			self.finish()
+		commands.setWalkVelocity(0.1, 0.5, 0.2);
 
-
-class RaiseArms(Node):
+class WalkRight(Node):
 	def run(self):
+		commands.setWalkVelocity(0.1, -0.5, -0.15);
 
-		global running_vx, running_vy, unseen_count
-		# get predicted location and velocity
-		ball = mem_objects.world_objects[core.WO_BALL]
+class TakeRest(Node):
+	def run(self):
+		commands.setWalkVelocity(0, 0, 0);
 
-		commands.setStiffness()
-		# pose.SittingPose()
-		# import pdb; pdb.set_trace()
-		distance = ball.distance
-		x = ball.loc.x
-		y = ball.loc.y
-		vx = ball.absVel.x
-		vy = ball.absVel.y
+class Goalie(Node):
+	actions = ['walk_right', 'take_rest']*5 + ['walk_left', 'take_rest']*10 + ['walk_right', 'take_rest']*5
+	count = 0
 
-		# running_vx = (1-FACTOR)*running_vx + FACTOR * vx
-		# running_vy = (1-FACTOR)*running_vy + FACTOR * vy
-
-		# v = math.sqrt(running_vx*running_vx+running_vy*running_vy)
-		v = math.sqrt(vx*vx+vy*vy)
-
-		# print (x, y, vx, vy)
-		
-		# y += 100
-
-		# print ("\n\n\n\n End y: ", end_y)
-		# print ("\n\n\n\n Vx: ", vx, "vy: ", vy, "End y: ", end_y, "V: ", v, "X: ", x)
-		# print ("\n\n\n\n bearing: ", ball.bearing)
-
-
-		if not ball.seen:
-			# print ("\n\n\nball not seen!\n\n\n")
-			# commands.setHeadPan(0, 0.1)
-			unseen_count += 1
-			choice = "nomove"
-			# running_vx = 0.0
-			# running_vy = 0.0
-			# print ("\n\n\n\n Ball moving!!! \n\n\n\n")
-		elif v < V_THRESHOLD:
-			commands.setHeadPan(ball.bearing, 0.05)
-			# print ("\n\n\nball steady\n\n\n")
-			choice = "nomove"
+	def run(self):
+		if self.count < len(self.actions):
+			action = self.actions[self.count]
 		else:
-			commands.setHeadPan(ball.bearing, 0.05)
-			# print ("\n\n\n ball moving!!!\n\n\n")
+			action = 'take_rest'
+		self.count += 1
 
-			head_angle = core.joint_values[core.HeadPan]
-			# print ("head angllllle!", head_angle)
+		robot = mem_objects.world_objects[memory.robot_state.WO_SELF]
 
-			t = 5.0
+		print ('\n\n\n', action, self.count, robot.loc.x, robot.loc.y, robot.orientation, '\n\n\n')
+		self.postSignal(action)
 
-			end_x = x + t * vx
-			end_y = y + t * vy
-
-
-
-
-			# if v > V_THRESHOLD and running_vx < 0 and (X_THRESHOLD - 100) <= x and (x <= X_THRESHOLD+ 100):
-			if end_x <= X_THRESHOLD:
-
-				if end_y <= -CENTER_MAX_THRESHOLD:
-					choice = "right"
-				elif end_y >= CENTER_MAX_THRESHOLD:
-					choice = "left"
-				elif end_y < CENTER_MAX_THRESHOLD and end_y > -CENTER_MAX_THRESHOLD:
-					choice = "center"
-				else:
-					choice = "nomove"
-
-				# end_y -= 50;
-				# if end_y < -CENTER_MIN_THRESHOLD and end_y > -CENTER_MAX_THRESHOLD:
-				# 	print ("\n\n\n\nleft!!!!!\n\n\n")
-				# 	choice = "left"
-				# elif end_y > CENTER_MIN_THRESHOLD and end_y < CENTER_MAX_THRESHOLD:
-				# 	print ("\n\n\n\nright!!!!!\n\n\n")
-				# 	choice = "right"
-				# elif end_y < CENTER_MIN_THRESHOLD and end_y > -CENTER_MIN_THRESHOLD:
-				# 	print ("\n\n\n\ncenter!!!!!\n\n\n")
-				# 	choice = "center"
-				# else:
-				# 	choice = "nomove"
-
-			else:
-				# print ("\n\n\n\nno move\n\n\n\n")
-				choice = "nomove"
-
-		if unseen_count >= 3:
-			unseen_count = 0
-			choice = "unseen"
-			# else:
-			# 	print ("\n\n\n\nno move\n\n\n\n")
-			# 	choice = "nomove"
-
-		self.postSignal(choice)
 		
+class TryPose(Node):
+	def run(self):
+		self.poseSignal("try")
 
 class Playing(LoopingStateMachine):
 	def setup(self):
-		raiseArm = RaiseArms()
-		sit = pose.SittingPose()
-		wait = Wait
-		arms = {
-			"left": pose.RaiseLeftArm(time=1.),
-			"right": pose.RaiseRightArm(time=1.),
-			"center": pose.RaiseBothArms(time=1.),
-			"unseen": NotSeen(),
-			"nomove": pose.SittingPose(time=0.3),
+		nodes = {
+			'turn_left' : TurnLeft(),
+			'turn_right' : TurnRight(),
+			'walk_front'  : WalkFront(),
+			'walk_back' : WalkBack(),
+			'walk_left'  : WalkLeft(),
+			'walk_right' : WalkRight(),
+			'take_rest' : TakeRest()
 		}
+		goalie = Goalie()
 
-		# arms = {"left": RaiseLeft(),
-		# "right": RaiseRight(),
-		# "center": RaiseBoth(),
-		# "not_seen": NotSeen()
-		# }
-		# self.add_transition(sit, T(0.5), pose.RaiseRightArm(), T(5), sit)
-		# self.trans(raiseArm, S("nomove"), sit, T(0.3), pose.RaiseBothArms(), T(5), raiseArm)
-
-		for direction in arms:
-			arm = arms[direction]
-			if direction in ["left", "right", "center"]:
-				self.add_transition(raiseArm, S(direction), arm, T(0.1), raiseArm)
-			else:
-				self.add_transition(raiseArm, S(direction), arm, T(0.1), raiseArm)
-
-			# if direction in ["left", "right", "center"]:
-				# self.add_transition(raiseArm, S(direction), arm, T(0.4), wait, C, raiseArm)
-			# else:
-				# self.add_transition(raiseArm, S(direction), arm, T(0.4), raiseArm)
+		for signal, node in nodes.iteritems():
+			self.add_transition(goalie, S(signal), node, T(DELAY), goalie)
